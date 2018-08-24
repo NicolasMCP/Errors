f39a9eae6a38dde20306c0564dd6a0082a8c7c1c
NicolasMCP <nicolas.mcp@gmail.com>
2018-08-24 17:59:33 -0300

    Ajustes nos escudos (graficos)

--- errors/recebe-erros.php
+++ errors/recebe-erros.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, user-scalable=yes, initial-scale=0.5, maximum-scale=1.0, minimum-scale=0.5">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Error</title>
 </head>
@@ -16,11 +16,11 @@ http_response_code($recebe);// Envia os dados no cabeçalho da resposta, suporte
 
 //echo "<h1 style='text-align:center'>Erro $recebe</h1>";
 
-//mostra o meme relacionado ao erro
+//mostra o escudo relacionado ao erro
 //echo "<img src=\"https://jul10l1r4.github.io/HTTP_-_Resposta/img/$recebe\">";
 echo "
 <div style=\"text-align: center;\">
-    <img src=\"/meme/errors/escudo$recebe.png\">
+    <img src=\"/errors/escudo$recebe.png\">
 </div>
 ";
 ?>
